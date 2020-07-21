24ab204fc48e380ab31a93223d58a835e2b64c8e
jayoharedee <jmorenstein@georgebrown.ca>
2020-07-21 01:40:05 -0400

    static pdf for testing

--- client/src/components/RenderPDF.js
+++ client/src/components/RenderPDF.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
-import { Document, Page } from 'react-pdf';
 
 import samplePDF from './sample.pdf';
+
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.1.266/pdf.worker.js`;
 
 export default function RenderPDF() {
   const [numPages, setNumPages] = useState(null);
